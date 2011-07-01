33f2c4d8af68a7dca5a82a35766293fad2949c89
JongMan Koo <jongman@gmail.com>
2011-07-01 15:29:32 -0500

    wiki read is in

--- algospot/wiki/views.py
+++ algospot/wiki/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 from models import Page, PageRevision
 
 def detail(request, slug):
+    import markdown
     page = Page.objects.get(slug=slug)
-    return render(request, "detail.html", {"page": page})
+    rendered = markdown.markdown(page.current_revision.text)
+    return render(request, "detail.html",
+            {"page": page,
+             "rendered_text": rendered})
