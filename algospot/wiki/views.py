a2618ac9e2463ed622ea616a44507e56ef338c67
JongMan Koo <jongman@gmail.com>
2011-07-01 16:42:02 -0500

    ok wiki edit works!! hahahaha

--- algospot/wiki/views.py
+++ algospot/wiki/views.py
@@ -1,5 +1,6 @@
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from models import Page, PageRevision
 from forms import EditForm
 
@@ -13,5 +14,11 @@ def detail(request, slug):
 
 def edit(request, slug):
     page = Page.objects.get(slug=slug)
-    form = EditForm({"text": page.current_revision.text})
+    form = EditForm(data=request.POST or None,
+            initial={"text": page.current_revision.text})
+
+    if request.method == "POST" and form.is_valid():
+        form.save(page, request.user)
+        return redirect(reverse("wiki-detail", kwargs={"slug": page.slug}))
+
     return render(request, "edit.html", {"page": page, "form": form})
