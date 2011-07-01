0165409e6c34cc5002c7f3145db50af48b48ef26
JongMan Koo <jongman@gmail.com>
2011-07-01 04:28:42 -0500

    oops I forgot to commit this

---
+++ algospot/wiki/urls.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls.defaults import *
+import views
+
+urlpatterns = patterns('wiki.views',
+        url(r'^(?P<slug>[^\.^/]+)/$', views.detail, name='wiki-detail'),
+        )
+
