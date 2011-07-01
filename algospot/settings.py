2688ecb909e96dfa6649cb61c4a3f833704f9bbe
JongMan Koo <jongman@gmail.com>
2011-07-01 04:12:26 -0500

    static files setup for dev

--- algospot/settings.py
+++ algospot/settings.py
@@ -73,9 +73,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    j("static"),
 )
 
 # List of finder classes that know how to find static files in
