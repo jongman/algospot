039c92065b48e4c2e76ea9d378c73bc8469fa020
JongMan Koo <jongman@gmail.com>
2011-07-03 01:55:24 -0500

    some template clean up and settings is working

---
+++ algospot/base/forms.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from django.contrib.auth.models import User
+from models import UserProfile
+
+class SettingsForm(forms.Form):
+    email = forms.EmailField(label=u"이메일", max_length=75)
+    intro = forms.CharField(label=u"자기소개", 
+            widget=forms.Textarea(attrs={"class": "large",
+                "rows": "5"}), required=False)
+
+    def save(self, user):
+        user.email = self.cleaned_data["email"]
+        user.get_profile().intro = self.cleaned_data["intro"]
+        user.save()
--- algospot/base/views.py
+++ algospot/base/views.py
@@ -1,7 +1,21 @@
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.http import HttpResponseForbidden
+from models import UserProfile
+from forms import SettingsForm
 
 def profile(request, user_id):
     pass
 
 def settings(request, user_id):
-    pass
+    user = get_object_or_404(User, id=user_id)
+    if request.user != user and not request.user.is_superuser:
+        return HttpResponseForbidden("Forbidden operation.")
+    form = SettingsForm(data=request.POST or None, initial={"email":
+        user.email, "intro": user.get_profile().intro})
+    if request.method == "POST" and form.is_valid():
+        form.save(user)
+        return redirect(reverse("user_profile", kwargs={"user_id": user_id}))
+    return render(request, "settings.html",
+            {"form": form, "settings_user": user})
