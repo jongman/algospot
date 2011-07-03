cad4df10ab10f15eb29ba6887acef09963727164
JongMan Koo <jongman@gmail.com>
2011-07-03 01:08:21 -0500

    didn't add base.urls

---
+++ algospot/base/urls.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls.defaults import *
+import views
+urlpatterns = patterns('base.views',
+        url(r'^profile/(?P<user_id>.+)$', views.profile, name='user_profile'),
+        url(r'^settings/(?P<user_id>.+)$', views.settings, name='user_settings'),
+    )
+
