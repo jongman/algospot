7e1ac0e40d887766f80048b5e2653ad6b130b0b0
JongMan Koo <jongman@gmail.com>
2011-07-06 02:43:40 -0500

    post writing and reading works

--- www/forum/urls.py
+++ www/forum/urls.py
@@ -6,9 +6,9 @@ urlpatterns = patterns('forum.views',
         url(r'^list/(?P<slug>.+)/(?P<page>[0-9]*)$', views.list, name='forum-list'),
         url(r'^read/(?P<id>[0-9]+)$', views.read, name="forum-read"),
         url(r'^edit/(?P<id>[0-9]+)$', views.write, name="forum-edit", 
-            kwargs={"slug": ""}),
+            kwargs={"slug": None}),
         url(r'^write/(?P<slug>.*)$', views.write, name="forum-write",
-            kwargs={"id": -1}),
+            kwargs={"id": None}),
         url(r'^delete/(?P<id>[0-9]+)$', views.delete, name="forum-delete"),
         )
 
--- www/forum/views.py
+++ www/forum/views.py
@@ -1,19 +1,55 @@
-from django.shortcuts import get_object_or_404
+# -*- coding: utf-8 -*-
+from django.shortcuts import get_object_or_404, render, redirect
+from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from djangoutils import get_or_none
 from models import Category, Post
-#from forms import EditForm
+from forms import WriteForm
 
 def list(request, slug, page):
     pass
 
 def read(request, id):
-    pass
+    post = get_object_or_404(Post, id=id)
+    category = post.category
+    breadcrumbs = [
+            (reverse("forum-list", 
+                kwargs={"slug": category.slug, "page": 1}), category.name),
+            (reverse("forum-read", kwargs={"id": id}), post.title)]
+    return render(request, "read.html", {"breadcrumbs": breadcrumbs, 
+        "post": post, "category": category})
 
 @login_required
 def write(request, slug, id):
-    return HttpResponse("ouch")
+    initial_data = {}
+    action = u"글 쓰기"
+    if slug != None:
+        category = get_object_or_404(Category, slug=slug)
+        initial_data["category"] = category.id
+    if id != None:
+        action = u"글 편집하기"
+        post = get_object_or_404(Post, id=id)
+        if not request.user.is_superuser and request.user != post.user:
+            return HttpForbidden("operation is forbidden.")
+        initial_data["title"] = post.title
+        initial_data["text"] = post.text
+        initial_data["category"] = post.category.id
+        category = post.category
+    breadcrumbs = [(reverse("forum-list",
+        kwargs={"slug": category.slug, "page": 1}),
+        category.name)]
+    if id != None:
+        breadcrumbs.append((reverse("forum-read", kwargs={"id": id}),
+            post.title))
+    breadcrumbs.append((None, action))
+
+    form = WriteForm(data=request.POST or None, initial=initial_data)
+    if request.method == "POST" and form.is_valid():
+        id = form.save(request.user, id)
+        return redirect(reverse("forum-read", kwargs={"id": id}))
+    return render(request, "write.html", {"form": form, "action": action,
+        "breadcrumbs": breadcrumbs, "category": category})
 
 def delete(request, id):
     pass
