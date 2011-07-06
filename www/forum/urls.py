e84c753e32b75e59f578620d6468875a9a56617d
JongMan Koo <jongman@gmail.com>
2011-07-06 01:31:59 -0500

    forum url scheme change

--- www/forum/urls.py
+++ www/forum/urls.py
@@ -5,8 +5,10 @@ import views
 urlpatterns = patterns('forum.views',
         url(r'^list/(?P<slug>.+)/(?P<page>[0-9]*)$', views.list, name='forum-list'),
         url(r'^read/(?P<id>[0-9]+)$', views.read, name="forum-read"),
-        url(r'^edit/(?P<id>[0-9]+)$', views.edit, name="forum-edit"),
-        url(r'^write/(?P<slug>.+)$', views.write, name="forum-write"),
+        url(r'^edit/(?P<id>[0-9]+)$', views.write, name="forum-edit", 
+            kwargs={"slug": ""}),
+        url(r'^write/(?P<slug>.*)$', views.write, name="forum-write",
+            kwargs={"id": -1}),
         url(r'^delete/(?P<id>[0-9]+)$', views.delete, name="forum-delete"),
         )
 
--- www/forum/views.py
+++ www/forum/views.py
@@ -1,14 +1,19 @@
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from djangoutils import get_or_none
+from models import Category, Post
+#from forms import EditForm
+
 def list(request, slug, page):
     pass
 
 def read(request, id):
     pass
 
-def edit(request, id):
-    pass
-
-def write(request, slug):
-    pass
+@login_required
+def write(request, slug, id):
+    return HttpResponse("ouch")
 
 def delete(request, id):
     pass
