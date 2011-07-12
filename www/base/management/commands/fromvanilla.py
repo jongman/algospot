85b4b377318bd05a173623070a62cf3e8e54d1af
JongMan Koo <jongman@gmail.com>
2011-07-12 00:56:01 -0500

    initial implementation of fromvanilla

--- www/base/management/commands/fromvanilla.py
+++ www/base/management/commands/fromvanilla.py
@@ -1,9 +1,41 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+import os.path
+import csv
+import json
 
-def migrate_user():
-    pass
+def parse_feed(home, filename):
+    names = [line.split("`")[1] 
+            for line in open(os.path.join(home, filename + ".sql")) 
+            if line.startswith("  `")]
+    for line in csv.reader(open(os.path.join(home, filename + ".txt"))):
+        yield dict(zip(names, line))
+
+def migrate_user(home):
+    username_seen = set()
+    created = 0
+    for u in parse_feed(home, "GDN_User"):
+        if u["Name"] in username_seen:
+            print "%s is a duplicate" % u["Name"]
+            continue
+        if u["Deleted"] == "1": continue
+        new_user = User(id=u["UserID"],
+                username = u["Name"],
+                date_joined=u["DateInserted"],
+                email=u["Email"],
+                is_active=True,
+                last_login=u["DateLastActive"],
+                password=u["Password"],
+                is_superuser=(u["Admin"] == "1"))
+        new_user.save()
+        created += 1
+        if created % 10 == 0:
+            print "created %d users so far" % created
+        username_seen.add(u["Name"])
+    print "created %d users." % created
+
+
 
 class Command(BaseCommand):
     args = '<csv dump directory>'
@@ -11,4 +43,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         dump_path = args[0]
-        migrate_user()
+        migrate_user(dump_path)
