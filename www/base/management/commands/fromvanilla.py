1161ccbb25bc656b091ba350ee52d08d4df68548
JongMan Koo <jongman@gmail.com>
2011-07-12 03:38:55 -0500

    fromvanilla reads from mysql directly.

--- www/base/management/commands/fromvanilla.py
+++ www/base/management/commands/fromvanilla.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-import os.path
-import csv
-import json
+from forum.models import Post, Category
+import MySQLdb
 
-def parse_feed(home, filename):
-    names = [line.split("`")[1] 
-            for line in open(os.path.join(home, filename + ".sql")) 
-            if line.startswith("  `")]
-    for line in csv.reader(open(os.path.join(home, filename + ".txt"))):
-        yield dict(zip(names, line))
+def fetch_all(db, table):
+    c = db.cursor()
+    c.execute("SELECT * FROM %s;" % table)
+    return c.fetchall()
 
-def migrate_user(home):
+def migrate_user(db):
     username_seen = set()
     created = 0
-    for u in parse_feed(home, "GDN_User"):
+    for u in fetch_all(db, "GDN_User"):
         if u["Name"] in username_seen:
             print "%s is a duplicate" % u["Name"]
             continue
@@ -28,7 +25,7 @@ def migrate_user(home):
                 date_joined=u["DateInserted"],
                 email=u["Email"],
                 is_active=True,
-                last_login=u["DateLastActive"],
+                last_login=u["DateLastActive"] or u["DateInserted"],
                 password=pw,
                 is_superuser=(u["Admin"] == "1"))
         new_user.save()
@@ -38,12 +35,37 @@ def migrate_user(home):
         username_seen.add(u["Name"])
     print "created %d users." % created
 
+CATEGORY_MAP = {"freeboard": "free",
+        "qna": "qna",
+        "openlecture": "old",
+        "contest": "old",
+        "algospot_contest": "old",
+        "editorial": "old",
+        "aoj_board": "qna",
+        "news": "news"
+        }
+
+def migrate_forum(db):
+    """
+    category_id_map = {}
+    for cat in parse_feed(home, "GDN_Category"):
+        slug = CATEGORY_MAP.get(cat["UrlCode"], None)
+        if not slug: continue
+        category_id_map[cat["CategoryID"]] = Category.objects.get(slug=slug)
+        """
+    pass
 
 
 class Command(BaseCommand):
-    args = '<csv dump directory>'
+    args = '<mysql host> <mysql user> <mysql password> <mysql db> [app]'
     help = 'Migrate data over from Vanilla\'s CSV dump'
 
     def handle(self, *args, **options):
-        dump_path = args[0]
-        migrate_user(dump_path)
+        host, user, password, db = args[:4]
+        db = MySQLdb.connect(host=host, user=user, passwd=password, db=db,
+                cursorclass=MySQLdb.cursors.DictCursor)
+        app = "all" if len(args) == 4 else args[4]
+        if app in ["all", "user"]:
+            migrate_user(db)
+        if app in ["all", "forum"]:
+            migrate_forum(db)
