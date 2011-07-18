b7c4236967b20cf31c63dc576f50808bdaa4dff8
JongMan Koo <jongman@gmail.com>
2011-07-18 15:10:12 -0500

    oops, edit/list view working now

--- www/judge/templatetags/judge_tags.py
+++ www/judge/templatetags/judge_tags.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django import template
+from ..models import Problem, Submission
 
 register = template.Library()
 
@@ -17,6 +18,25 @@ def percentage(parser, token):
     toks = token.split_contents()
     a, b = toks[1:3]
     return PercentNode(a, b)
+
+class HasSolvedNode(template.Node):
+    def __init__(self, problem, user, result):
+        self.problem = template.Variable(problem)
+        self.user = template.Variable(user)
+        self.result = result
+    def render(self, context):
+        problem = self.problem.resolve(context)
+        user = self.user.resolve(context)
+        ret = (Submission.objects.filter(problem=problem, user=user,
+                                         state=Submission.ACCEPTED).count() > 0)
+        context[self.result] = ret
+        return ""
+
+@register.tag
+def get_has_solved(parser, token):
+    toks = token.split_contents()
+    problem, by, user, as_, solved = toks[1:]
+    return HasSolvedNode(problem, user, solved)
 
 @register.filter
 def print_length(length):
