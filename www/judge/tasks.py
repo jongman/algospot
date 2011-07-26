3518a80fbb15223661b5fe12f37757283671d44c
JongMan Koo <jongman@gmail.com>
2011-07-26 13:51:38 -0500

    testing judge tasks: working

--- www/judge/tasks.py
+++ www/judge/tasks.py
@@ -1,12 +1,5 @@
 from celery.decorators import task
-import time
 
-@task
-def receive_submission(submission):
-    time.sleep(3)
-    get_judge_data.delay(submission)
-
-@task
-def get_judge_data(submission):
-    time.sleep(3)
-
+@task()
+def add(x, y):
+    return x + y
