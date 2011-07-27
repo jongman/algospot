32aa219ea9b45c89b0ec8b6790034024810fdb53
JongMan Koo <jongman@gmail.com>
2011-07-27 13:20:33 -0500

    removed arkose: will use a custom sandbox

--- get_dependencies.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p www/libs/external/
-cd www/libs/external/
-wget http://launchpad.net/arkose/trunk/1.2.2/+download/arkose-1.2.2.tar.gz
-tar -xvzf arkose-1.2.2.tar.gz
-ln -sf arkose-1.2.2/arkose .
