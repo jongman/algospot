cca9d9be7b4a3ab90aa46dfc80a13000eabeabbe
JongMan Koo <jongman@gmail.com>
2012-10-24 22:00:49 -0500

    techplanet banners

--- www/base/processors.py
+++ www/base/processors.py
@@ -1,10 +1,21 @@
 from datetime import datetime
 
-CAMPAIGNS = [{'name': 'codesprint', 
-              'start': datetime(2012, 10, 8, 9, 0, 0),
-              'end': datetime(2012, 11, 4, 23, 59, 59),
-              'image': '/static/images/banners/codesprint.gif',
-              'link': 'http://www.codesprint.kr/'}]
+CAMPAIGNS = [
+    {
+        'name': 'techplanet', 
+        'start': datetime(2012, 10, 8, 9, 0, 0),
+        'end': datetime(2012, 10, 25, 23, 59, 59),
+        'image': '/static/images/banners/techplanet.jpg',
+        'link': 'http://www.techplanet.kr/'
+    },
+    {
+        'name': 'codesprint', 
+        'start': datetime(2012, 10, 8, 9, 0, 0),
+        'end': datetime(2012, 11, 4, 23, 59, 59),
+        'image': '/static/images/banners/codesprint.gif',
+        'link': 'http://www.codesprint.kr/'
+    },
+]
 
 def select_campaign(request):
     campaign = None
