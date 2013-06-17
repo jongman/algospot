77f293854b05596e4f6fc473546073c1ffe8e1f8
JongMan Koo <jongman@gmail.com>
2013-06-16 21:22:57 -0500

    codesprint 2013

--- www/base/processors.py
+++ www/base/processors.py
@@ -15,6 +15,13 @@ CAMPAIGNS = [
         'image': '/static/images/banners/codesprint.gif',
         'link': 'http://www.codesprint.kr/'
     },
+    {
+        'name': 'codesprint2013',
+        'start': datetime(2013, 6, 18, 0, 0, 0),
+        'end': datetime(2013, 7, 14, 0, 0, 0),
+        'image': '/static/images/banners/codesprint2013.png',
+        'link': 'http://codesprint.skplanet.com/'
+    }
 ]
 
 def select_campaign(request):
