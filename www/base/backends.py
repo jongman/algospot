b1cbd945bbc725860b2942c7c7093cf50ec32924
Algospot Production <algospot-admins@andromeda-express.com>
2013-06-17 11:24:59 +0900

    support for duplicate emails

--- www/base/backends.py
+++ www/base/backends.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib.auth.models import User
-from djangoutils import get_or_none
 import hashlib
 from django.contrib.auth.backends import ModelBackend
 
@@ -41,6 +40,12 @@ def get_hash(password, stored):
         hash = md5(hash + password)
     return stored[:12] + encode64(hash, 16)
 
+def first_or_none(model, **kwargs):
+    instances = model.objects.filter(**kwargs)
+    if not instances: return None
+    return instances[0]
+
+
 MAGIC = r"sha1$deadbeef$"
 class LegacyBackend(ModelBackend):
     supports_object_permissions = False
@@ -48,13 +53,13 @@ class LegacyBackend(ModelBackend):
     supports_inactive_user = False
 
     def get_user(self, user_id):
-        return get_or_none(User, pk=user_id)
+        return first_or_none(User, pk=user_id)
 
     def authenticate(self, username=None, password=None):
         if not username or not password: return None
-        user = get_or_none(User, username=username)
+        user = first_or_none(User, username=username)
         if not user:
-            user = get_or_none(User, email=username)
+            user = first_or_none(User, email=username)
         if not user: return user
         if user.password.startswith(MAGIC):
             stored = user.password[len(MAGIC):]
@@ -65,7 +70,7 @@ class LegacyBackend(ModelBackend):
 
 class EmailBackend(ModelBackend):
     def authenticate(self, username=None, password=None):
-        user = get_or_none(User, email=username)
+        user = first_or_none(User, email=username)
         if not user: return user
         if user.check_password(password):
             return user
