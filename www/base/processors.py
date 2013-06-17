5982bf599f8b65f1638114faf01889859a40372c
Algospot Production <algospot-admins@andromeda-express.com>
2013-06-17 11:24:36 +0900

    campaigns

--- www/base/processors.py
+++ www/base/processors.py
@@ -15,6 +15,20 @@ CAMPAIGNS = [
         'image': '/static/images/banners/codesprint.gif',
         'link': 'http://www.codesprint.kr/'
     },
+    {
+        'name': 'nexon_global_internship', 
+        'start': datetime(2012, 12, 4, 0, 0, 0),
+        'end': datetime(2012, 12, 25, 23, 59, 59),
+        'image': '/static/images/banners/nexon-gi.jpg',
+        'link': 'http://www.nexonin.com/'
+    },
+    {
+        'name': 'jmbook', 
+        'start': datetime(2012, 11, 15, 0, 0, 0),
+        'end': datetime(2013, 12, 31, 23, 59, 59),
+        'image': '/static/images/banners/jmbook.png',
+        'link': 'http://book.algospot.com/'
+    },
 ]
 
 def select_campaign(request):
