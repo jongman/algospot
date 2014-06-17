81085cca7adcb49f00f2d9862b45b6eb52efbb18
JongMan Koo <jongman@gmail.com>
2014-06-17 23:52:52 +0900

    disable date field in problem search index

--- www/judge/search_indexes.py
+++ www/judge/search_indexes.py
@@ -3,7 +3,7 @@ from models import Problem
 
 class ProblemIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.EdgeNgramField(document=True, use_template=True)
-    date = indexes.DateTimeField(model_attr='updated_on')
+    # date = indexes.DateTimeField(model_attr='updated_on')
 
     def get_model(self):
         return Problem
