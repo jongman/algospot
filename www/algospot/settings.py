4a800473879a6bb7a62a991d6f88e81a322507c0
JongMan Koo <jongman@gmail.com>
2014-06-18 23:50:13 +0900

    media is relocated to /www-media so we can make www-data own it

--- www/algospot/settings.py
+++ www/algospot/settings.py
@@ -57,7 +57,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = j("../media")
+MEDIA_ROOT = j("/www-media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
