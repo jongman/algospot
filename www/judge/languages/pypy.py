c790e0f6bb8f18fe35cc7c4c7a62a6914f5b2ea7
JongMan Koo <jongman@gmail.com>
2014-06-24 17:33:36 +0900

    pypy bindings

---
+++ www/judge/languages/pypy.py
@@ -0,0 +1,23 @@
+import subprocess
+
+def system(cmd):
+    return subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE).communicate()
+
+LANGUAGE = "Python 2 (PyPy)"
+EXT = "pypy"
+VERSION = system(["pypy", "-V"])[1].split("\n")[1]
+ADDITIONAL_FILES = []
+
+def setup(sandbox, source_code):
+    sandbox.write_file(source_code, "submission.py")
+    return {"status": "ok"}
+
+def run(sandbox, input_file, time_limit, memory_limit):
+    result = sandbox.run("pypy submission.py", stdin=input_file, time_limit=time_limit,
+                         override_memory_limit=memory_limit,
+                         stdout=".stdout", stderr=".stderr")
+    toks = result.split()
+    if toks[0] != "OK":
+        return {"status": "fail", "message": result, "verdict": toks[0] }
+    return {"status": "ok", "time": toks[1], "memory": toks[2], "output": ".stdout"}
