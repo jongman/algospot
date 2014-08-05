9888b7f90624da9d176ffd001f9d5e83e7ff25b6
JongMan Koo <jongman@gmail.com>
2014-08-05 22:48:49 +0900

    add prefix to events, allow for manual curation

--- utils/ical_generator/merge.py
+++ utils/ical_generator/merge.py
@@ -1,7 +1,15 @@
+#!env python
 import icalendar
-import sys
 import datetime
 import pytz
+import urllib2
+
+CALENDARS = {
+    '[CF] ': 'https://www.google.com/calendar/ical/br1o1n70iqgrrbc875vcehacjg%40group.calendar.google.com/public/basic.ics',
+    '[TC] ': 'https://www.google.com/calendar/ical/appirio.com_bhga3musitat85mhdrng9035jg%40group.calendar.google.com/public/basic.ics',
+    '[GCJ] ': 'https://www.google.com/calendar/ical/google.com_jqv7qt9iifsaj94cuknckrabd8%40group.calendar.google.com/public/basic.ics',
+    '': 'https://www.google.com/calendar/ical/iqe3kkmf7vltagnjs7fd2sv4a8%40group.calendar.google.com/public/basic.ics'
+}
 
 merged = icalendar.Calendar()
 merged.add('PRODID', '-//Algospot Calendar//algospot.com//EN')
@@ -15,10 +23,12 @@ def recent_enough(c):
         dt_start = dt_start.date()
     return dt_start >= thresh
 
-for fn in sys.argv[1:]:
-    cal = icalendar.Calendar.from_ical(open(fn).read())
+for prefix, url in CALENDARS.items():
+    ics = urllib2.urlopen(url).read()
+    cal = icalendar.Calendar.from_ical(ics)
     for c in cal.subcomponents: 
-        if recent_enough(c):
+        if recent_enough(c) and 'SUMMARY' in c:
+            c['SUMMARY'] = prefix + c['SUMMARY']
             merged.add_component(c)
 
 print merged.to_ical()
