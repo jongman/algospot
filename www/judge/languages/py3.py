ac596932419a102e306116991f7c4917c5e4670f
sunghyunzz <me@sunghyun.kitchen>
2015-01-10 12:29:34 +0900

    added support for Python3
    though python3 is already installed, I added python3 in ansible setting file just to make sure

---
+++ www/judge/languages/py3.py
@@ -0,0 +1,26 @@
+import subprocess
+
+
+def system(cmd):
+    return subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE).communicate()
+
+LANGUAGE = "Python 3"
+EXT = "py3"
+VERSION = system(["python3", "--version"])[0].split('\n')[0]
+ADDITIONAL_FILES = []
+
+
+def setup(sandbox, source_code):
+    sandbox.write_file(source_code, "submission.py")
+    return {"status": "ok"}
+
+
+def run(sandbox, input_file, time_limit, memory_limit):
+    result = sandbox.run("python3 submission.py", stdin=input_file, time_limit=time_limit,
+                         memory_limit=memory_limit,
+                         stdout=".stdout", stderr=".stderr")
+    toks = result.split()
+    if toks[0] != "OK":
+        return {"status": "fail", "message": result, "verdict": toks[0] }
+    return {"status": "ok", "time": toks[1], "memory": toks[2], "output": ".stdout"}
