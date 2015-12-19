de299b1653c7c6400f8091bcb8ac943edaf96982
JongMan Koo <jongman@gmail.com>
2015-12-19 00:51:49 -0500

    캠페인 여러개 지원

--- www/base/processors.py
+++ www/base/processors.py
@@ -81,12 +81,11 @@ CAMPAIGNS = [
 ]
 
 def select_campaign(request):
-    campaign = None
+    campaigns = []
     now = datetime.now()
     for c in CAMPAIGNS:
         if c['start'] <= now <= c['end']:
-            campaign = c
-            break
-    return {'campaign': campaign}
+            campaigns.append(c)
+    return {'campaigns': campaigns}
     
 
