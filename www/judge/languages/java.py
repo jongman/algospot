33afb22080240c9f7984e9d953a8457e86390d7a
imyoyo <imyoyo.suhwan@gmail.com>
2016-08-02 18:49:05 +0900

    자바의 메모리 제한 정책 개선
    sandbox 의 메모리 감시를 완화하고, 문제의 제한조건을 JVM 에게 맡김

--- www/judge/languages/java.py
+++ www/judge/languages/java.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import subprocess
 from django.conf import settings
 
@@ -24,9 +25,12 @@ def setup(sandbox, source_code):
     return {"status": "ok"}
 
 def run(sandbox, input_file, time_limit, memory_limit):
-    result = sandbox.run("java -Xmx%dK -Xms%dK Main" % (memory_limit, memory_limit), 
+    # 크기를 예측하기 어려윤 JVM의 non-heap 메모리 영역을 확보해 주기 위해 sandbox 의 메모리 제한을 충분히 크게 할당한다.
+    # 문제의 메모리 제한 조건은 java 의 -Xmx 옵션으로 heap 영역에만 적용되는 셈인데, 충분히 납득할 만 하다고 본다.
+    max_non_heap_size = 65536
+    result = sandbox.run("java -Xmx%dK -Xms%dK Main" % (memory_limit, memory_limit),
                          stdin=input_file, time_limit=time_limit,
-                         memory_limit=memory_limit,
+                         memory_limit=memory_limit + max_non_heap_size,
                          stdout=".stdout", stderr=".stderr")
     toks = result.split()
     if toks[0] != "OK":
